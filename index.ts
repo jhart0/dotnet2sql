e4bb17d92255dd8eb6b5f043c2a4bbd3f4942b61
Josh Hart <jhart.guitar@gmail.com>
2020-07-07 20:43:53 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+export default class dotnet2sql {
+    static getSqlTypeFromDotnet(typeName: string) {
+        switch(typeName) { 
+            case 'long':
+            case 'Int64':
+               return 'bigint'
+            case 'int':
+            case 'Int32':
+                return 'int'
+            case 'short':
+            case 'Int16':
+                return 'smallint'
+            case 'byte':
+                return 'tinyint'
+            case 'bool':
+            case 'Boolean':
+                return 'bool'
+            case 'string':
+            case 'String':
+                return 'nvarchar'
+            case 'decimal':
+            case 'Decimal':
+                return 'decimal'
+            case 'DateTime':
+                return 'datetime'
+         }
+    }
+}
